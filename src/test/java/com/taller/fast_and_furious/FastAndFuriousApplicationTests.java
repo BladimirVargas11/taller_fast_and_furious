cadf0fe3c562376e0f9841f36e3bea89181ad778
BladimirVargas11 <bjvargas@unicesar.edu.co>
2024-09-30 22:11:58 -0500

    first commit

---
+++ src/test/java/com/taller/fast_and_furious/FastAndFuriousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.fast_and_furious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastAndFuriousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
